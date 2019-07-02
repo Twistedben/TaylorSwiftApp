3a842a3e54472a89a7e1fa9feb9dbaaa43b1c453
Benjamin Broestl <Bensgamingmail@yahoo.com>
2019-07-01 20:09:20 -0400

    Doing initial coommit for Albums app in Udemy course

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+// HEader component for top of app
+// Import libraries for making component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make component
+const Header = () => {
+  return <Text>Albums</Text>; 
+}; 
+
+// Make component available to other parts of app
+export default Header;
